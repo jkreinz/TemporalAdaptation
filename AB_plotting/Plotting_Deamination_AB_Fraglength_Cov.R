6f80b761983fd079874f629cd17b9a83dada3311
Julia Kreiner <julia.kreiner@mail.utoronto.ca>
2022-09-14 15:38:56 -0700

    Update Plotting_Deamination_AB_Fraglength_Cov.R

--- AB_plotting/Plotting_Deamination_AB_Fraglength_Cov.R
+++ AB_plotting/Plotting_Deamination_AB_Fraglength_Cov.R
@@ -1,10 +1,16 @@
-#compiled output from MapDamage of deamination levels
-ct<-read.table("CtoT.all")
 
 library(ggplot2)
-meta<-read.csv("Downloads/Curated Herbarium Tissue Samples - Sheet12.csv")
+library(tidyverse)
+
+#MapDamage compiled output
+ct<-read.table("CtoT.all")
 names(ct)<-c("readpos","CtoT","Sample")
-ct$Sample<-gsub("HB0","HBO",ct$Sample)
+ct$Sample<-gsub("HBO","HB0",ct$Sample)
+
+#more by sample (meta)data
+meta<-read.csv("Herbarium_CovReadStats.csv")
+
+#merge
 ct_meta<-inner_join(meta,ct,by="Sample")
 
 v1<-ggplot(data=ct_meta, aes(readpos, CtoT, color=Collection.Year, group=Sample)) +
@@ -14,17 +20,6 @@ v1<-ggplot(data=ct_meta, aes(readpos, CtoT, color=Collection.Year, group=Sample)
   xlab("Read Position from 5p") +
   theme_bw() +
   scale_color_viridis_c()
-
-ga<-read.table("GtoA.all")
-ggplot(data=ga, aes(V1, V2, color=V3)) +
-  geom_point(alpha=.5, cex=2) +
-  geom_line(alpha=.3) +
-  ylab("Percent G to A Deamination") +
-  xlab("Read Position from 3p") +
-  theme_bw() +
-  theme(legend.position = "none")   
-
-library(tidyverse)
 
 
 v2<-ct_meta %>% filter(readpos==1) %>%
@@ -49,8 +44,8 @@ frag<-read.csv("Downloads/Curated Herbarium Tissue Samples - Sheet15.csv")
 names(frag)[1]<-"Sample"
 frag_meta<-inner_join(meta,frag,by="Sample")
 
-v4<-frag_meta %>% 
-  ggplot(aes(Collection.Year, fragmentlength, color=Collection.Year)) +                              
+v4<-ct_meta %>% 
+  ggplot(aes(Collection.Year, FragLength, color=Collection.Year)) +                              
   ylab("Fragment Length") +
   geom_smooth(method="lm") +
   geom_point(alpha=.8, cex=2) + 
@@ -61,30 +56,31 @@ lm1<-lm(data=frag_meta, FinalCov~Collection.Year)
 summary(lm1)
 anova(lm1)
 
-library(gridExtra)
-library(lemon)
-grid_arrange_shared_legend(v1,v2,v3,v4, ncol=2 , nrow=2,position = "bottom")
 
+#allelic bias calculations for two datasets - Ancestry Informative SNPs and focal AG alleles
 AB_head<-read.table("AB_headerorder.txt")
 AB_alt<-read.table("alternate_AB_bygeno.txt")
 AB_ref<-read.table("reference_AB_bygeno.txt")
-AB_anc<-read.table("herbarium_ancestyrinformative_ABbygeno.txt")ymu
+AB_anc<-read.table("herbarium_ancestyrinformative_ABbygeno.txt")
 
+#merge ref and alt ag alleles to get the set of all focal 154 alleles
 AB_all<-rbind(AB_alt,AB_ref)
+#get sample means of allelic bias
 AB_focalmean<-colMeans(AB_all[,-c(1:4)],na.rm = T)
+#get sample standard errors of allelic bias
 AB_focalse<-sapply(AB_all[,-c(1:4)],function(x)sd(x,na.rm = T)/sqrt(length(na.omit(x))))
 
+#same for Ancestry informative alleles
 AB_ancmean<-colMeans(AB_anc[,-c(1:4)],na.rm = T)
 AB_ancse<-sapply(AB_anc[,-c(1:4)],function(x)sd(x,na.rm = T)/sqrt(length(na.omit(x))))
 
+#merge into DF
 df2<-data.frame("Sample"=AB_head, "AB_focal"=AB_focalmean, "AB_anc"=AB_ancmean,
                 "AB_focal_se"=AB_focalse,
                 "AB_anc_se"=AB_ancse)
 names(df2)[1]<-"Sample"
-df2$Sample<-gsub("HB0","HBO",df2$Sample)
+df2$Sample<-gsub("HBO","HB0",df2$Sample)
 AB_meta<-inner_join(df2,meta,by="Sample")
-
-AB_meta2<-AB_meta[complete.cases(AB_meta[,c(1:5)]),]
 
 v5<-ggplot(data=AB_meta, aes(Collection.Year, AB_focal, color=Collection.Year)) +
   geom_point() +
@@ -104,10 +100,9 @@ v6<-ggplot(data=AB_meta, aes(Collection.Year, AB_anc, color=Collection.Year)) +
   geom_errorbar(ymin=AB_meta$AB_anc-AB_meta$AB_anc_se,ymax=AB_meta$AB_anc+AB_meta$AB_anc_se) +
   labs(y="Mean Allelic Bias by Sample\n(Ancestry Informative Alleles)")
 
+#test association with time
 summary(lm1<-lm(data=AB_meta, AB_focal ~ Collection.Year))
 summary(lm1<-lm(data=AB_meta, AB_anc ~ Collection.Year))
-cor(AB_meta$AB_anc, AB_meta$Collection.Year)
-cor(AB_meta$AB_focal, AB_meta$Collection.Year, use="complete.obs")
 
 library(cowplot)
 # extract the legend from one of the plots
